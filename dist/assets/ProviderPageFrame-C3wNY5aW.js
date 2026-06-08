0c607fb9efb37a62064db8f7a450432a8bf4cea6
AYESIGA ISAAC <ayesigai921@gmail.com>
2026-06-09 01:56:01 +0300

    commit

--- dist/assets/ProviderPageFrame-C3wNY5aW.js
+++
@@ -1,2 +0,0 @@
-import{p as o}from"./vendor-CSoTYNU9.js";const t=(...r)=>r.filter(Boolean).join(" ");function i({children:r,className:e,maxWidthClassName:a}){return o.jsx("div",{className:t("fh-page-frame fh-grid-start",a,e),children:r})}export{i as P};
-//# sourceMappingURL=ProviderPageFrame-C3wNY5aW.js.map
